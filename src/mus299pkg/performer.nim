d576c696ac4f3003d47d4c27c381c3292a1482bf
Jason Yip <jayip2@illinois.edu>
2024-01-26 17:59:44 -0600

    feat: add basic `Performer` functionality and `Performer` subclass definitions

---
+++ src/mus299pkg/performer.nim
@@ -0,0 +1,27 @@
+import core
+import pool
+
+import std/asyncdispatch
+import std/[sets, tables]
+import std/sequtils
+
+proc dec[A](t: var CountTable[A]; key: A; val = 1) =
+  if val > 0:
+    assert t.getOrDefault(key) >= val
+  t.inc(key, -val)
+
+proc performTask*(performer: var PerformerObj; task: Task; pool: var TaskPool) {.async.} =
+  assert performer.state != Performing
+  assert anyIt({
+      performer.categories: false,
+      task.allowedCategories: false,
+      performer.categories * task.allowedCategories: true,
+      }, (it[0].len > 0) == it[1])
+  performer.state = Performing
+  performer.currentTasks.add(task)
+  pool.availableTasks.dec(task)
+
+  # Do performing here
+
+proc fetchTask*(performer: var PerformerObj; pool: var TaskPool): Future[Task] {.async.} =
+  let task = await pool.popTask(performer.categories)
