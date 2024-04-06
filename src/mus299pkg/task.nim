0ea4a22b2464c720bf1d47ea5a7d971dc6d521fb
Jason Yip <jayip2@illinois.edu>
2024-04-06 12:46:05 -0500

    feat: check for cycles when task's dependencies change after creation

--- src/mus299pkg/task.nim
+++ src/mus299pkg/task.nim
@@ -3,6 +3,7 @@ import std/[strutils, unicode]
 import std/sets
 import std/re
 import std/streams
+import std/sequtils
 
 import chronos
 import chronos/asyncproc
@@ -64,9 +65,35 @@ proc newTaskSnippet*(snippet: string;
   await resyncTaskSnippet(result, pool, isExpression, isAssignment, staffJoinStr, varName, sourceTemplate, staffTemplate)
 
 
+proc changeDependencies*(task: Task, newDepends: HashSet[Task]) =
+  let e = ValueError.newException("task dependency cycle detected")
+  if task in newDepends:
+    raise e
+  var
+    acyclic: HashSet[Task]
+    stack = toSeq(newDepends - task.depends)
+
+  while stack.len > 0:
+    let cur = stack.pop()
+    if cur notin acyclic:
+      acyclic.incl(cur)
+      for v in cur.depends.items:
+        if v == task:
+          raise e
+        if v notin acyclic:
+          stack.add(v)
+
+  for i in items(task.depends - newDepends):
+    i.dependents.excl(task)
+
+  for i in items(newDepends - task.depends):
+    i.dependents.incl(task)
+
+  task.depends = newDepends
 
 
 template newTask*(snippet: TaskSnippet; args: varargs[typed]): Task =
   result = Task(snippet: snippet, args)
+
   for parent in result.depends:
     parent.dependents.incl(result)
