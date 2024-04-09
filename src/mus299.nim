1c0c9475a3955e28c410f6606cefbd51db5d7bef
Jason Yip <jayip2@illinois.edu>
2024-04-09 16:23:53 -0500

    feat: update main code

--- src/mus299.nim
+++ src/mus299.nim
@@ -2,6 +2,10 @@ import mus299pkg/[core, task, pool, performer]
 import std/appdirs
 import std/re
 import std/paths
+import std/sets
+import std/random
+
+import chronos
 
 
 
@@ -12,13 +16,35 @@ const
 
   dataDir = getDataDir() / "mus299".Path
 
-  fluidsynth = "fluidsynth"
-  # audio drivers alsa, pulseaudio and sdl2 allow concurrent
-  fluidsynthParams = @["-i", "-l", "/usr/share/soundfonts/freepats-general-midi.sf2"]
+  player = "aplaymidi"
+  playerParams = @["-p", "128", "-d", "0"]
+
+# TODO: start fluidsynth daemon and stop for convenience if asked
 
 
-let
-  isExpression = re(r"^" & expressionRe, {reIgnoreCase, reMultiLine, reStudy})
-  isAssignment = re(r"^([a-z]+)\s*=" & expressionRe, {reMultiLine, reStudy})
-  sourceTemplate = readFile(string(dataDir / "template.ly".Path))
-  staffTemplate = readFile(string(dataDir / "staff.ly".Path))
+proc main {.async.} =
+
+  randomize()
+
+  let
+    isExpression = re(r"^" & expressionRe, {reIgnoreCase, reMultiLine, reStudy})
+    isAssignment = re(r"^([a-z]+)\s*=" & expressionRe, {reMultiLine, reStudy})
+    sourceTemplate = readFile(string(dataDir / "template.ly".Path))
+    staffTemplate = readFile(string(dataDir / "staff.ly".Path))
+
+    brass = toHashSet(["Brass".Category])
+    instrument = newInstrument("acoustic grand", "", 0)
+
+  var
+    pool = TaskPool()
+    performer = Performer(categories: brass, name: "Trumpet", instrument: instrument)
+  pool.performers.incl(performer)
+  var
+    task = Task(snippet: (await newTaskSnippet("a b c d", pool, isExpression, isAssignment, staffJoinStr, varName, sourceTemplate, staffTemplate)), allowedCategories: brass)
+
+  pool.addTask(task)
+  await performer.perform(pool, player, playerParams) do (x: Task):
+    echo x.repr
+  
+when isMainModule:
+  waitFor main()
