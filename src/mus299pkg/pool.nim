be70d313c3a8128eb6403d5841d6fa109acb7e0e
Jason Yip <jayip2@illinois.edu>
2024-04-10 21:24:13 -0500

    fix: ensure proper reincarnation task dependents

--- src/mus299pkg/pool.nim
+++ src/mus299pkg/pool.nim
@@ -2,19 +2,16 @@ import std/[sets, tables]
 import std/random
 from std/sugar import collect
 import std/enumerate
-import std/options
 
 import chronos
 
 import core
 
 
-proc randomItem[T](pool: var Table[Category, HashSet[T]];
-                   categories: HashSet[Category];
-                   delete: proc(x: T): bool {.noSideEffect, closure, raises: [].} = nil
-                  ): Option[T] {.raises: [].} =
-
-  var possible: HashSet[T]
+proc wouldWait[T](pool: var Table[Category, HashSet[T]];
+                  categories: HashSet[Category];
+                  delete: proc(x: T): bool {.noSideEffect, closure, raises: [].} = nil
+                 ): bool {.raises: [].} =
   for category in categories.items:
     pool.withValue(category, entry):
       if not delete.isNil():
@@ -28,23 +25,30 @@ proc randomItem[T](pool: var Table[Category, HashSet[T]];
           pool.del(category)
           continue
 
+      if entry[].len > 0:
+        return false
+
+  true
+
+
+proc randomItem[T](pool: var Table[Category, HashSet[T]];
+                   categories: HashSet[Category];
+                  ): T {.raises: [].} =
+
+  var possible: HashSet[T]
+  for category in categories.items:
+    pool.withValue(category, entry):
       possible.incl(entry[])
 
-  if possible.len > 0:
-    let chosen = rand(possible.len - 1)
-    for i, item in enumerate(possible.items):
-      if i == chosen:
-        return item.some()
-  else:
-    return none(T)
+  let chosen = rand(possible.len - 1)
+  for i, item in enumerate(possible.items):
+    if i == chosen:
+      return item
 
 
 proc wakeupNext(pool: TaskPool; categories: HashSet[Category]) {.raises: [].} =
-  let chosenWaiter = randomItem(pool.getters, categories) do (x: Future[void].Raising([CancelledError])) -> bool:
-    x.finished()
-
-  chosenWaiter.map do (x: auto):
-    x.complete()
+  if not (wouldWait(pool.getters, categories) do (x: Future[void].Raising([CancelledError])) -> bool: x.finished()):
+    randomItem(pool.getters, categories).complete()
 
 
 proc addTask*(pool: TaskPool; task: Task) =
@@ -54,21 +58,21 @@ proc addTask*(pool: TaskPool; task: Task) =
 
 proc popTask*(pool: TaskPool; categories: HashSet[Category]; performer: Performer): Future[Task] {.async: (raises: [CancelledError]).} =
 
-  while true:
+  proc reincarnate =
+    pool.toReincarnate.withValue(performer, entry):
+      for t in entry[].dependents.items:
+        t.dependents.excl(entry[])
+        pool.addTask(t)
+      pool.toReincarnate.del(performer)
 
-    let chosenTask = randomItem(pool.pool, categories)
-    if chosenTask.isSome:
-      result = chosenTask.unsafeGet
-      break
+
+  if wouldWait(pool.pool, categories):
 
     let getter = Future[void].Raising([CancelledError]).init("TaskPool.addTask")
     for category in categories.items:
       pool.getters.mgetOrPut(category, HashSet[Future[void].Raising([CancelledError])].default).incl(getter)
 
-    pool.toReincarnate.withValue(performer, entry):
-      for t in entry[].dependents.items:
-        t.dependents.excl(entry[])
-        pool.addTask(t)
+    reincarnate()
 
     try:
       await getter
@@ -76,7 +80,10 @@ proc popTask*(pool: TaskPool; categories: HashSet[Category]; performer: Performe
       if not getter.cancelled():
         pool.wakeupNext(categories)
       raise exc
+  else:
+    reincarnate()
 
+  result = randomItem(pool.pool, categories)
   for category in result.allowedCategories:
     var taskSet: ptr HashSet[Task]
     try:
